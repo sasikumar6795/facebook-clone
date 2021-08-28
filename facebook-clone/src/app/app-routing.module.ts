50ffdde3ce8c351dcaa8536318ece6a39db2da11
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-08-28 15:34:25 +0530

    folder Structure for the project

--- facebook-clone/src/app/app-routing.module.ts
+++ facebook-clone/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { FacebookGuard } from './guards/facebook.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //lazy loaded route, unless we are logged in this home component will not be loaded
+  {
+    path : '',
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule),
+    canActivate: [FacebookGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
